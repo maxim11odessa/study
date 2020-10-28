6db42157b0410b48e4c939e5f2b7cb016b437e69
Koniev <maxim11.odessa@gmail.com>
2020-10-28 20:12:37 +0200

    1

---
+++ script.js
@@ -0,0 +1,17 @@
+const numberOfFilms = +prompt("Сколько фильмоа вы уже посмотрели?", "");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const a = prompt("Один из просмотренный фильмов?", ""),
+  b = prompt("На сколько вы оцените его?", ""),
+  c = prompt("Один из просмотренный фильмов?", ""),
+  d = prompt("На сколько вы оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[a] = b;
+console.log(personalMovieDB);
