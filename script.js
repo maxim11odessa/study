cd5606d805d8d062d7c6b506dc16b34299fbd095
Koniev <maxim11.odessa@gmail.com>
2020-10-28 20:31:31 +0200

    1

--- script.js
+++ script.js
@@ -13,5 +13,5 @@ const a = prompt("Один из просмотренный фильмов?", "")
   d = prompt("На сколько вы оцените его?", "");
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
