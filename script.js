aa33ae76adc13e4bb6e85d57a5e0f3b615094cb5
Koniev <maxim11.odessa@gmail.com>
2020-10-29 09:54:24 +0200

    1

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const numberOfFilms = +prompt("Сколько фильмоа вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмоа вы уже посмотрели?", "");
 
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-const a = prompt("Один из просмотренный фильмов?", ""),
-  b = prompt("На сколько вы оцените его?", ""),
-  c = prompt("Один из просмотренный фильмов?", ""),
-  d = prompt("На сколько вы оцените его?", "");
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+// const a = prompt("Один из просмотренный фильмов?", ""),
+//   b = prompt("На сколько вы оцените его?", ""),
+//   c = prompt("Один из просмотренный фильмов?", ""),
+//   d = prompt("На сколько вы оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+
+
+const numm = 5;
+(numm === 50) ? console.log('Ok') : console.log('Error');
