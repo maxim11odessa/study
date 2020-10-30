64737a286ac99138c284983dc288804197f78f09
Koniev <maxim11.odessa@gmail.com>
2020-10-30 16:50:00 +0200

    4

--- script.js
+++ script.js
@@ -1,66 +1,70 @@
-// let numberOfFilms;
+'use strict';
 
-// function start() {
-//    numberOfFilms = +prompt("Сколько фильмоа вы уже посмотрели?", "");
-//    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) 
-//       numberOfFilms = +prompt("Сколько фильмоа вы уже посмотрели?", "");
-//    }
-// }
+let personalMovieDB = {
+   count: 0,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+   start: function () {
+      personalMovieDB.count = +prompt("Сколько фильмоа вы уже посмотрели?", "");
+      while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+         personalMovieDB.count = +prompt("Сколько фильмоа вы уже посмотрели?", "");
+      }
+   },
+   rememberMyFilms: function () {
+      for (let i = 0; i < 2; i++) {
+         const a = prompt("Один из просмотренный фильмов?", ""),
+            b = prompt("На сколько вы оцените его?", "");
+         if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+         } else {
+            console.log('Error');
+            i--;
+         }
+      }
+   },
+   detectPersonalLevel: function () {
+      if (personalMovieDB.count < 10) {
+         console.log('Просмотрено довольно мало фильмов');
+      } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+         console.log('Вы классический зритель');
+      } else if (personalMovieDB.count > 30) {
+         console.log('Вы киноман');
+      } else {
+         console.log('Произошла ошибка!');
+      }
+   },
+   showMyDB: function (hidden) {
+      if (!hidden) {
+         console.log(personalMovieDB);
+      }
+   },
+   toggleVisibleMyDB: function(){
+      if (personalMovieDB.privat){
+         personalMovieDB.privat = false;
+      } else {
+         personalMovieDB.privat = true;
+      }
+   },
+   writeYourGenres: function () {
+      for (let i = 1; i <= 3; i++) {
+         let genre = prompt(`Ваш любимый жанр?: ${i}`);
+         if (genre === '' || genre == null){
+            console.log('Вы ввели данные не корректно или не ввели из вовсе');
+            i--;
+         } else {
+            personalMovieDB.genres[i - 1] = genre;
+         }
+         
+      }
+      personalMovieDB.genres.forEach((item, i) => {
+         console.log(`Любимый жанр ${i+1}- это ${item}`);
+      });
+   }
+};
 
-// start();
 
-// let personalMovieDB = {
-//    count: numberOfFilms,
-//    movies: {},
-//    actors: {},
-//    genres: [],
-//    privat: false,
-// };
-
-// function rememberMyFilms() {
-//    for (let i = 0; i < 2; i++) {
-//       const a = prompt("Один из просмотренный фильмов?", ""),
-//          b = prompt("На сколько вы оцените его?", "");
-//       if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//          personalMovieDB.movies[a] = b;
-//       } else {
-//          console.log('Error');
-//          i--;
-//       }
-//    }
-// }
-// rememberMyFilms();
-
-
-// function detectPersonalLevel() {
-//    if (personalMovieDB.count < 10) {
-//       console.log('Просмотрено довольно мало фильмов');
-//    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-//       console.log('Вы классический зритель');
-//    } else if (personalMovieDB.count > 30) {
-//       console.log('Вы киноман');
-//    } else {
-//       console.log('Произошла ошибка!');
-//    }
-// }
-// detectPersonalLevel();
-
-// function showMyDB(hidden) {
-//    if (!hidden){
-//       console.log(personalMovieDB);
-//    }
-// }
-// showMyDB(personalMovieDB.privat);
-
-// function writeYourGenres() {
-//    for (let i = 1; i <= 3; i++) {
-//       let genre = prompt(`Ваш любимый жанр?: ${i}`);
-//       personalMovieDB.genres[i - 1] = genre;
-
-
-//    }
-// }
-// writeYourGenres();
 // const numm = 5;
 // (numm === 50) ? console.log('Ok') : console.log('Error');
 
@@ -76,9 +80,9 @@
 // console.log(options.name);
 
 
-let str = prompt("", "");
-let products = str.split(", ");
-console.log(products);
+// let str = prompt("", "");
+// let products = str.split(", ");
+// console.log(products);
 
 
-console.log(Object.assign())
+// console.log(Object.assign())
